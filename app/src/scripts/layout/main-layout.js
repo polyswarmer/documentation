c0256e65bfd01577d912e6dfe0a242fc189199d7
josh <joshc@polyswarm.io>
2018-06-24 04:33:42 -0700

    [wip](scroll-spy): adds ul and li tags

--- app/src/scripts/layout/main-layout.js
+++ app/src/scripts/layout/main-layout.js
@@ -1,11 +1,14 @@
 import MainMenu from './main-menu';
+import ScrollSpy from './scroll-spy';
 
 export default class MainLayout {
   constructor() {
     this.mainMenu = new MainMenu();
+    this.scrollSpy = new ScrollSpy();
   }
 
   init() {
     this.mainMenu.init();
+    this.scrollSpy.init();
   }
 }
